57e29d01475ff09308fb15518e9220d8b6a99201
Max Verbitskiy <maxxtor@yandex.ru>
2023-02-26 14:55:40 +0400

    initial backend commit

---
+++ backend/foodgram/settings.py
@@ -0,0 +1,110 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    default='a+uyo%)tq%n$^uo&xei41!2f4zp0&2o=s(vhh=u!o_!*!qz(ij')
+
+DEBUG = True
+
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', default='*')]
+
+AUTH_USER_MODEL = 'users.User'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
+    'djoser',
+    'api',
+    'users',
+    'recipes',
+    'colorfield',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'foodgram.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'foodgram.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='foodgram'),
+        'USER': os.getenv('POSTGRES_USER', default='foodgram'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='1105'),
+        'HOST': os.getenv('DB_HOST', default='localhost'),
+        'PORT': os.getenv('DB_PORT', default=5432)
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'ru-RU'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MAX_LEN_HEX_FIELD = 7
+MAX_LEN_MODEL_FIELD = 200
+MIN_COOKING_TIME = 1
+MIN_INGR_AMOUNT = 1
---
+++ backend/recipes/models.py
@@ -0,0 +1,317 @@
+"""Модуль для создания, настройки и управления моделями приложения `recipes`.
+
+Models:
+    Ingredient:
+        Модель описания ингредиентов.
+        Связана с Recipe через модель IngredientInRecept (ManyToMany).
+    Tag:
+       Модель описания тэгов для группировки и сортировки рецептов.
+       Связана с Recipe (ManyToMany).
+    Recipe:
+        Основная модель, описывающая рецепты.
+    IngredientInRecept:
+        Модель для связи Recipe и Ingredient с указанием количества ингредиентов.
+    Favorite:
+        Модель для связи Recipe и User. Определяет избранные рецепты для пользователя.
+    Cart:
+        Модель для связи Recipe и User. Определяет рецепты в списке покупок пользователя.
+"""
+from colorfield.fields import ColorField
+from django.conf import settings
+from django.core.validators import MinValueValidator, RegexValidator
+from django.db import models
+
+
+class Ingredient(models.Model):
+    """Ингредиент для рецептов.
+
+    Attributes:
+        name(str):
+            Название ингредиента.
+            Ограничение по длине (200).
+        measurement_unit(str):
+            Единица измерения ингредента.
+            Ограничение по длине (200).
+    """
+    name = models.CharField(
+        'Название ингредиента',
+        max_length=settings.MAX_LEN_MODEL_FIELD,
+    )
+    measurement_unit = models.CharField(
+        'Единица измерения ингредиента',
+        max_length=settings.MAX_LEN_MODEL_FIELD,
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        ordering = ('name',)
+        #Требуется ли проверка уникальности? UniqueConstrant?
+        #Требуется ли проверка пустоты?
+
+    def __str__(self):
+        return f'{self.name} ({self.measurement_unit})'
+
+    #Требуется ли перевод в lower_case? Переопределить def clean?
+
+
+class Tag(models.Model):
+    """Тег для рецептов.
+
+    Attributes:
+        name(str):
+            Название тега рецепта.
+            Ограничение по длине (200), уникальные.
+        color(str):
+            Цвет тега в HEX.
+            Ограничение по длине (7), уникальные.
+        slug(str):
+            Слаг тега репета.
+            Ограничение по длине (200), уникальные.
+    """
+    name = models.CharField(
+        'Название тега',
+        max_length=settings.MAX_LEN_MODEL_FIELD,
+        db_index=True,
+        unique=True,
+    )
+    color = ColorField(
+        'Цвет тега в HEX',
+        format='hex',
+        max_length=settings.MAX_LEN_HEX_FIELD,
+        unique=True,
+        # Переписать hex_color_validator?
+        validators=[
+            RegexValidator(
+                regex="^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$",
+            )
+        ],
+        error_messages={'validators': 'Проверьте формат ввода'},
+    )
+    slug = models.SlugField(
+        'Slug тега',
+        max_length=settings.MAX_LEN_MODEL_FIELD,
+        db_index=True,
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ('name',)
+
+    def __str__(self):
+        return f'{self.name} (цвет: {self.color})'
+
+    #Требуется ли перевод в lower_case? Переопределить def clean?
+
+
+class Recipe(models.Model):
+    """Рецепты.
+
+    Attributes:
+        author(int):
+            Автор рецепта.
+            Связь ForeignKey с моделью User.
+        name(str):
+            Название рецепта.
+            Ограничение по длине (200).
+        image(str):
+            Изображение рецепта.
+            Ссылка (url) на изображение.
+        text(str):
+            Описание рецепта.
+        ingredients(int):
+            Ингредиенты для рецепта.
+            Связь ManyToMany с моделью Ingredient через модель IngredientInRecept.
+        tags(array[int]):
+            Список тегов рецепта.
+            Связь ManyToMany с моделью Tag.
+        cooking_time(int):
+            Время приготовления рецепта.
+            Ограничение по минимальному значению (1).
+        pub_date(datetime):
+            Дата добавления рецепта.
+            Присваивается автоматически.
+    """
+    author = models.ForeignKey(
+        'Автор рецепта',
+        related_name='recipes',
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    name = models.CharField(
+        'Название рецепта',
+        related_name='recipes',
+        max_length=settings.MAX_LEN_MODEL_FIELD,
+    )
+    image = models.ImageField(
+        'Изображение рецепта',
+        related_name='recipes',
+        upload_to='recipes/',
+    )
+    text = models.TextField(
+        'Описание рецепта',
+        related_name='recipes',
+    )
+    ingredients = models.ManyToManyField(
+        'Ингредиенты для рецепта',
+        related_name='recipes',
+        to=Ingredient,
+        through='IngredientInRecept',
+    )
+    tags = models.ManyToManyField(
+        'Тег рецепта',
+        related_name='recipes',
+        to=Tag,
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        'Время приготовления по рецепту',
+        validators=(MinValueValidator(settings.MIN_COOKING_TIME)),
+        error_messages={'validators':
+                        'Минимальное время приготовления 1 минута'},
+        default=1,
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления рецепта',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+        ordering = ('-pub_date',)
+        # Проверить на уникальность для автора? UniqueConstraint?
+
+    def __str__(self):
+        return f'{self.name} (Автор: {self.author.username})'
+
+    # Требуется ли перевод в name в capitalize? Переопределить def clean?
+    # Требуется ли уменьшенная версия изображения? Переопределить def save?
+
+
+class IngredientInRecept(models.Model):
+    """Количество ингредиентов в рецепте.
+    Связь моделей Recipe и Ingredient с указанием количества ингредиентов.
+
+    Attributes:
+        recipe(int):
+            Рецепт.
+            Связь ForeignKey с моделью Recipe.
+        ingredients(int):
+            Ингредиент.
+            Связь ForeignKey с моделью Ingredient.
+        amount(int):
+            Количиство ингредиентов в рецепте.
+            Ограничение по минимальному значению (1).
+    """
+    recipe = models.ForeignKey(
+        'Связанный рецепт',
+        related_name='ingredient',
+        to=Recipe,
+        on_delete=models.CASCADE,
+    )
+    ingredients = models.ForeignKey(
+        'Связанные ингредиенты',
+        related_name='recipe',
+        to=Ingredient,
+        on_delete=models.CASCADE,
+    )
+    amount = models.PositiveSmallIntegerField(
+        'Количество ингредиентов',
+        validators=(MinValueValidator(settings.MIN_INGR_AMOUNT)),
+        error_messages={'validators':
+                        'В рецепте должен быть как минимум 1 ингредиент'}
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Количество ингредиентов'
+        ordering = ('-id', )
+        # Требуется ли проверка на уникальность ингредиента в рецепте? UniqueConstrant?
+
+    def __str__(self):
+        return f'{self.amount} {self.ingredients} в {self.recipe}'
+
+
+class Favorite(models.Model):
+    """Избранные рецепты.
+    Связь моделей Recipe и User.
+
+    Attributes:
+        recipe(int):
+            Рецепт.
+            Связь ForeignKey с моделью Recipe.
+        user(int):
+            Пользователь.
+            Связь ForeignKey с моделью User.
+        add_date(datetime):
+            Дата добавления в избранное.
+    """
+    recipe = models.ForeignKey(
+        'Избранный рецепт',
+        related_name='favorites',
+        to=Recipe,
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        'Пользователь',
+        related_name='favorites',
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    add_date = models.DateTimeField(
+        'Дата добавления в избранное',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = 'Избранный рецепт'
+        verbose_name_plural = 'Избранные рецепты'
+        ordering = ('-add_date',)
+        # Требуется ли проверка на уникальность рецепта в избранных? UniqueConstrant?
+
+    def __str__(self):
+        return f'{self.recipe} нравится {self.user}'
+
+
+class Cart(models.Model):
+    """Список покупок.
+    Связь моделей Recipe и User.
+
+    Attributes:
+        recipe(int):
+            Рецепт.
+            Связь ForeignKey с моделью Recipe.
+        user(int):
+            Пользователь.
+            Связь ForeignKey с моделью User.
+        add_date(datetime):
+            Дата добавления в список покупок.
+    """
+    recipe = models.ForeignKey(
+        'Рецепты в списке покупок',
+        related_name='carts',
+        to=Recipe,
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        'Пользователь',
+        related_name='carts',
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    add_date = models.DateTimeField(
+        'Дата добавления в список покупок',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт в списке покупок'
+        verbose_name_plural = 'Рецепты в списке покупок'
+        ordering = ('-add_date',)
+        # Требуется ли проверка на уникальность рецепта в списке? UniqueConstrant?
+
+    def __str__(self):
+        return f'{self.recipe} в списке {self.user}'
